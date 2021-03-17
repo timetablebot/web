d1d5dd538d8cab5f922252c15a3d2b35136e8c9c
LukBukkit <luk.bukkit@gmail.com>
2021-03-17 22:44:31 +0100

    Uses the .env if no .env.local file exists

--- posthtml.config.js
+++ posthtml.config.js
@@ -1,25 +1,19 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
 
-if (fs.existsSync('.env.local')) {
-
-    const envConfig = dotenv.parse(fs.readFileSync('.env.local'));
-    for (let k in envConfig) {
-        process.env[k] = envConfig[k];
-    }
-
-}
+const file = fs.existsSync('.env.local') ? '.env.local' : '.env';
+const env = dotenv.parse(fs.readFileSync(file));
 
 module.exports = {
     plugins: {
         'posthtml-expressions': {
             locals: {
-                IMPRINT_NAME: process.env.IMPRINT_NAME,
-                IMPRINT_STREET: process.env.IMPRINT_STREET,
-                IMPRINT_CITY: process.env.IMPRINT_CITY,
-                IMPRINT_COUNTRY: process.env.IMPRINT_COUNTRY,
-                IMPRINT_TELEPHONE: process.env.IMPRINT_TELEPHONE,
-                IMPRINT_EMAIL: process.env.IMPRINT_EMAIL,
+                IMPRINT_NAME: env.IMPRINT_NAME,
+                IMPRINT_STREET: env.IMPRINT_STREET,
+                IMPRINT_CITY: env.IMPRINT_CITY,
+                IMPRINT_COUNTRY: env.IMPRINT_COUNTRY,
+                IMPRINT_TELEPHONE: env.IMPRINT_TELEPHONE,
+                IMPRINT_EMAIL: env.IMPRINT_EMAIL,
             },
         },
     },
